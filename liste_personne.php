00ed0ee2c109b071952ec7114e8514f61e716c33
Aekyu <Baptiste.clms@gmail.com>
2021-12-08 08:56:01 +0100

    08/12/2021

--- liste_personne.php
+++ liste_personne.php
@@ -1,20 +1,19 @@
-<?PHP
+<?php
 //require
-require "ressources.php";
-//connection
-$mysqli = new mysqli($servername, $username, $password, $database);
+    //connection
+$mysqli = new mysqli('localhost', 'root', '', 'projet_noel');
 //
- 
+
 $query = "select * from donneur";
 $result = $mysqli->query($query);
 $total = mysqli_num_rows($result);
 $tableau_non_melanger = [];
 if($total > 0) {
-    while($row = mysqli_fetch_array($result)){
-        $nom_non_melanger = $row['nom'];
+    while($row = mysqli_fetch_assoc($result)){
+        $nom_non_melanger = $row;
         $tableau_non_melanger[] = $nom_non_melanger;
     }
 }
-//
-print(json_encode($tableau_non_melanger));
+echo json_encode($tableau_non_melanger);
+
 ?>
