f39d48b2c022cda0ed38860228b6158ce3b90257
Aekyu <Baptiste.clms@gmail.com>
2021-12-13 19:12:34 +0100

    a

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let form = document.querySelector("#form");
 let inputName = document.querySelector("#Name");
 let errorName = document.querySelector("#errorName");
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    console.log("ok")
-    let div = document.createElement("div");
-                if (inputName.value.trim() === "") {
-                    errorName.textContent = "vous devez saisir un nom"
-                    errorName.style.display = "block"
-                }
-            })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("ok");
+  let div = document.createElement("div");
+  if (inputName.value.trim() === "") {
+    errorName.textContent = "vous devez saisir un nom";
+    errorName.style.display = "block";
+  }
+});
