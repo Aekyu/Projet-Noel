893baf544694f5f28b16f7fdd82f4d6bf5a0497d
Aekyu <Baptiste.clms@gmail.com>
2021-12-13 15:02:29 +0100

    13-12-2021

--- page_tas.php
+++ page_tas.php
@@ -5,11 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
 <body>
+    <div class="d-flex justify-content-around flex-wrap w-50">
+        <div class="d-flex justify-content-around w-100">
+            <div id="bloc">
+                <div>Donneur</div>
+            </div>
+            <div id="bloc">
+                <div>Receveur</div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-around w-100">
+            <div id="donne"></div>
+            <div id="recev"></div>
+        </div>
+        <div>
+            <button id="tas1">Tirer au sort</button>
+        </div>
+    </div>
 
+
+
+
+    <div>
+        <img src=" cadeau.jpg" id="font" alt="">
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
